5e65e0e7b6ef781713246f08e17129af617e87ab
Christian Stprix <christprix@gmail.com>
2023-06-27 21:42:42 +0900

    whew

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,10 +2,10 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    question = params[:question]
-    if question == "I am going to work"
+    @question = params[:question]
+    if @question == "I am going to work"
       @answer = 'Great!'
-    elsif question[-1] == "?"
+    elsif @question[-1] == "?"
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
